fea3f40a9bfe7b7ccec390bbc08e9920b99335fe
Zach Taylor <ztaylor@cmc.edu>
2022-10-08 17:17:06 -0700

    Initial commit from Create Next App

---
+++ pages/index.tsx
@@ -0,0 +1,9 @@
+export default function Index() {
+    return (
+        <>
+            <h1>Hello world!</h1>
+            <p>This is a starter template you can use for Web Dev for Makers projects.</p>
+            <img src="/logo.png" alt="Web Dev for Makers Logo" />
+        </>
+    );
+}
